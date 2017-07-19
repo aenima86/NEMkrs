bf1d4b56a2a39b373fa0109d39eb3440c5116916
aenima86 <appsileci@gmail.com>
2017-07-19 21:16:48 +0200

    Add files via upload

--- js/scriptMulti.js
+++ js/scriptMulti.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-
-
 	// Load nem-browser library
 	var nem = require("nem-sdk").default;
 
@@ -12,182 +10,19 @@ $(document).ready(function () {
 	var transferTransaction = nem.model.objects.get("transferTransaction");
 
 	// Get an empty common object to hold pass and key
-	var common = nem.model.objects.get("common");
+	var common = nem.model.objects.get("common");var common = nem.model.objects.get("common");
 
-
-	// Create random bytes from PRNG
-	var rBytes = nem.crypto.nacl.randomBytes(32);
-
-	// Convert the random bytes to hex
-	var randomSeed = uaConv(rBytes);
-	$('#rndseed').text(randomSeed);
-
-	//check if we are in recovery mode
-	urlCheck();
-
-
-
-	// Convert the random bytes to hex
-	var privateKey = nem.utils.convert.ua2hex(rBytes);
-
-	var keyPair = nem.crypto.keyPair.create(privateKey);
-
-	var publicKey = keyPair.publicKey.toString();
-
-	var address = nem.model.address.toAddress(publicKey, nem.model.network.data.mainnet.id); //nem.model.network.data.mainnet.id  nem.model.network.data.testnet.id
-
-
-
-
-
-	$("#startBtn").click(function(){
-		$("#section1").hide();
-		$("#section2").fadeIn();
-
-		$(window).scrollTop(0);
-		
-	});
-	$("#nextbtnSec2").click(function(){
-
-		var privateKey = $('#privatekeyMultisig').val();
-
-		// Check private key for errors
-		if (privateKey.length !== 64 && privateKey.length !== 66) return alert('Invalid private key, length must be 64 or 66 characters !');
-		if (!nem.utils.helpers.isHexadecimal(privateKey)) return alert('Private key must be hexadecimal only !');
-
-		$("#section2").hide();
-		$("#section3").fadeIn();
-
-		
-		$('#PK4C').text($('#privatekeyMultisig').val());
-
-		$('#q1').text($('#question1').find(":selected").text());
-		$('#q2').text($('#question2').find(":selected").text());
-		$('#q3').text($('#question3').find(":selected").text());
-		$('#q4').text("E-mail");
-
-		$('#a1').text($('#answer1').val());
-		$('#a2').text($('#answer2').val());
-		$('#a3').text($('#answer3').val());
-		$('#a4').text($('#answer4').val());
-
-		$(window).scrollTop(0);
-		
-	});
-	$("#backbtnSec3").click(function(){
-		$("#section3").hide();
-		$("#section2").fadeIn();
-
-		$(window).scrollTop(0);
-		
-	});
-	$("#nextbtnSec3").click(function(){
-		$("#section3").hide();
-		$("#section4").fadeIn();
-
-		$(window).scrollTop(0);
-
-		// Create key 1
-		var rBytes1 = nem.crypto.nacl.randomBytes(32);
-		var privateKey1 = nem.utils.convert.ua2hex(rBytes1);
-		var keyPair1 = nem.crypto.keyPair.create(privateKey1);
-		var publicKey1 = keyPair1.publicKey.toString();
-
-
-		// Create key 2
-		var rBytes2 = nem.crypto.nacl.randomBytes(32);
-		var privateKey2 = nem.utils.convert.ua2hex(rBytes2);
-		var keyPair2 = nem.crypto.keyPair.create(privateKey2);
-		var publicKey2 = keyPair2.publicKey.toString();
-
-		// Create key 3 (recovery key)
-		var passphrase = $('#rndseed').text()+$('#answer1').val().toLowerCase()+$('#answer2').val().toLowerCase()+$('#answer3').val().toLowerCase()+$('#answer4').val().toLowerCase();
-		var privateKey3 = makePrivateKey(passphrase);
-		var keyPair3 = nem.crypto.keyPair.create(privateKey3);
-		var publicKey3 = keyPair3.publicKey.toString();
-
-		$('#pk1').text(privateKey1);
-		$('#pk2').text(privateKey2);
-
-
-		var pathname = window.location.href;
-		var uri = pathname + '?seed=' + $('#rndseed').text() + '&Q1=' + $('#question1').find(":selected").text() + '&Q2=' + $('#question2').find(":selected").text() + '&Q3=' + $('#question3').find(":selected").text()+ '&Q4=Your e-mail address?';
-		$('#wh').text(encodeURI(uri.replace("multisig.htm", "index.htm")));
-
-
-
-		var PKmultisig = $('#privatekeyMultisig').val();
-
-
-		convert2multisig(PKmultisig,publicKey1,publicKey2,publicKey3);
-
-		
-	});
-
-
-
-	function uaConv(ua) {
-		var s = '';
-		var _hexEncodeArrayy = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-		for (var i = 0; i < ua.length; i++) {
-			var code = ua[i];
-			s += _hexEncodeArrayy[code >>> 4];
-			s += _hexEncodeArrayy[code & 0x0F];
-		}
-		return s;
-	};
-
-	function makePrivateKey(passphrase) {
-
-		var privateKey = nem.crypto.helpers.derivePassSha(passphrase, 6000).priv;
-		return privateKey;
-	};
-
-	function urlParam(name) {
-
-		try {
-			var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-			return results[1] || 0;
+	// Set default amount in view. It is text input so we can handle dot and comma as decimal mark easily (need cleaning but provided by the library)
 	
-		}
-		catch(err) {
-			
-		}
 
 
-	}
-
-	function urlCheck() {
-
-		if( urlParam('seed') != null) {
-			$("#section1").hide();
-			$("#section2").fadeIn();
-
-			$('#rndseed').text(decodeURI(urlParam('seed')));
-
-			var select = document.getElementById('question1');
-			select.options[select.options.length] = new Option(decodeURI(urlParam('Q1')));
-			select.selectedIndex = select.options.length-1;
-			$('#question1').attr("disabled", "disabled");
-
-			var select = document.getElementById('question2');
-			select.options[select.options.length] = new Option(decodeURI(urlParam('Q2')));
-			select.selectedIndex = select.options.length-1;
-			$('#question2').attr("disabled", "disabled");
-
-			var select = document.getElementById('question3');
-			select.options[select.options.length] = new Option(decodeURI(urlParam('Q3')));
-			select.selectedIndex = select.options.length-1;
-			$('#question3').attr("disabled", "disabled");
-
-
-		}
-		
-	};
-
-
-	function convert2multisig(key4conv,pub1,pub2,pub3) {
 	
+
+	/**
+     * Build transaction from form data and send
+     */
+	function send(key4conv,pub1,pub2,pub3) {
+
 
 		// Set the private key in common object
 		common.privateKey = key4conv; 
@@ -220,7 +55,6 @@ $(document).ready(function () {
 		obj1.deadline = transactionEntity.deadline
 
 
-
 		// Serialize transfer transaction and announce
 		nem.model.transactions.send(common, obj1, endpoint).then(function(res){
 			// If code >= 2, it's an error
@@ -232,8 +66,80 @@ $(document).ready(function () {
 		}, function(err) {
 			alert(err);
 		});
+	}
+
+
+
+	// Call start function
+	$("#startBtn").click(function() {
+		$("#section1").hide();
+		$("#section2").fadeIn();
+
+		$(window).scrollTop(0);
+	});
+
+	 
+	$("#nextbtnSec2").click(function() {
+		var privateKey = $('#privatekeyMultisig').val();
+
+		// Check private key for errors
+		//if (privateKey.length !== 64 && privateKey.length !== 66) return alert('Invalid private key, length must be 64 or 66 characters !');
+		//if (!nem.utils.helpers.isHexadecimal(privateKey)) return alert('Private key must be hexadecimal only !');
+
+		$("#section2").hide();
+		$("#section3").fadeIn();
 
 		
-		
-	};
+		$('#PK4C').text($('#privatekeyMultisig').val());
+
+		$('#q1').text($('#question1').find(":selected").text());
+		$('#q2').text($('#question2').find(":selected").text());
+		$('#q3').text($('#question3').find(":selected").text());
+		$('#q4').text("E-mail");
+
+		$('#a1').text($('#answer1').val());
+		$('#a2').text($('#answer2').val());
+		$('#a3').text($('#answer3').val());
+		$('#a4').text($('#answer4').val());
+
+		$(window).scrollTop(0);
+	});
+
+	$("#backbtnSec3").click(function() {
+		$("#section3").hide();
+		$("#section2").fadeIn();
+
+		$(window).scrollTop(0);
+	});
+
+	// Call send function when click on send button
+	$("#nextbtnSec3").click(function() {
+		var pub1 ='d6343879a5634610132b58948ff0aa32d36f364fca7c59058d9b21ed9ba846ff';
+		var pub2='05cf7da0bc154c598c394aaf9501cffaec39a3bd92fef884ab8df440439c12e1';
+		var pub3='cc61676a4275abcffd10a9ea1081091ff054a1a8a720429256aebf8034aab099';
+	  //send('913e172803bb22eb0f06bf90d9d52da066b55a376a451ecea691069b14e10580',pub1,pub2,pub3);
+	/*
+	  // Create key 1
+		var rBytes1 = nem.crypto.nacl.randomBytes(32);
+		var privateKey1 = nem.utils.convert.ua2hex(rBytes1);
+		var keyPair1 = nem.crypto.keyPair.create(privateKey1);
+		var pub1 = keyPair1.publicKey.toString();
+		// Create key 2
+		var rBytes2 = nem.crypto.nacl.randomBytes(32);
+		var privateKey2 = nem.utils.convert.ua2hex(rBytes2);
+		var keyPair2 = nem.crypto.keyPair.create(privateKey2);
+		var pub2 = keyPair2.publicKey.toString();
+		// Create key 2
+		var rBytes3 = nem.crypto.nacl.randomBytes(32);
+		var privateKey3 = nem.utils.convert.ua2hex(rBytes3);
+		var keyPair3 = nem.crypto.keyPair.create(privateKey3);
+		var pub3 = keyPair3.publicKey.toString();*/
+
+		var PKmultisig = $('#privatekeyMultisig').val();
+
+		send(PKmultisig,pub1,pub2,pub3);
+	});
+
+
+
 });
